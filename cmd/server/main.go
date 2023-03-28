b7c5838471e1adf8712d7df9353372ea7581bcda
Herbert Paes <herbert_pbarreto@hotmail.com>
2023-03-28 16:00:23 -0300

    adding Go-Chi

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/hpaes/api-project-golang/configs"
 	"github.com/hpaes/api-project-golang/internal/entity"
 	"github.com/hpaes/api-project-golang/internal/infra/database"
 	"github.com/hpaes/api-project-golang/internal/infra/webservers/handlers"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,6 +28,9 @@ func main() {
 	productDb := database.NewProductRepository(db)
 	productHandler := handlers.NewProductHandler(productDb)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8080", nil)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Timeout(300 * time.Millisecond))
+	router.Post("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8080", router)
 }
